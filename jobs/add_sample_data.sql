c194e02eef8e220f8799f4d3962c080c027c1228
sfc-gh-sajain <samit.jain@snowflake.com>
2026-07-28 01:32:08 +0000

    Push Changes

--- jobs/add_sample_data.sql
+++ jobs/add_sample_data.sql
@@ -4,4 +4,5 @@ INSERT INTO HELLO_WORLD_DB.DEMO_SCHEMA.HELLO_WORLD (FIRST_NAME, LAST_NAME, EMAIL
 VALUES
     ('Alice', 'Johnson', 'alice@example.com', '555-0101'),
     ('Bob', 'Smith', 'bob@example.com', '555-0202'),
+    ('Bob1', 'Smith1', 'bob1@example.com', '556-0202'),
     ('Charlie', 'Brown', 'charlie@example.com', '555-0303');
