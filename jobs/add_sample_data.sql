47280b3f6b546258ee8164266cd65aa22d525e87
sfc-gh-sajain <samit.jain@snowflake.com>
2026-07-28 18:49:21 +0000

    added new rows via git workspace

--- jobs/add_sample_data.sql
+++ jobs/add_sample_data.sql
@@ -5,4 +5,5 @@ VALUES
     ('Alice', 'Johnson', 'alice@example.com', '555-0101'),
     ('Bob', 'Smith', 'bob@example.com', '555-0202'),
     ('Samit', 'Jain', 'sjain@example.com', '717-0202'),
+    ('Mark', 'Mayo', 'mark@example.com', '444-0202'),
     ('Charlie', 'Brown', 'charlie@example.com', '555-0303');
